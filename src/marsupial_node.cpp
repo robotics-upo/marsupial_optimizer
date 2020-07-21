8b4bf925b3f56be35f5dbb101d54f82d962e5011
SaimonMR <simon.martinez.rzs@gmail.com>
2020-07-21 10:19:54 +0200

    Adding Dynamic Reconfigure

--- src/marsupial_node.cpp
+++ src/marsupial_node.cpp
@@ -1,13 +1,16 @@
 #include "marsupial.hpp"
 #include "ros/ros.h"
-
+#include <dynamic_reconfigure/server.h>
+#include "marsupial_g2o/OptimizationParamsConfig.h"
 
 // ============= Global Variables ================ 
 
 
 // =========== Function declarations =============
 
-
+void dynamicReconfigureCallback(marsupial_g2o::OptimizationParamsConfig &config, uint32_t level){
+  ROS_INFO("Dynamic reconfigure callback");
+}
 
 // =============== Main function =================
 
@@ -23,7 +26,13 @@ int main(int argc, char** argv) {
   Marsupial m_(nh, pnh, "marsupial_node");
   // ros::Rate r(ros::Duration(10));
   ros::Rate r(5);
-  
+
+  dynamic_reconfigure::Server<marsupial_g2o::OptimizationParamsConfig> server_;
+  dynamic_reconfigure::Server<marsupial_g2o::OptimizationParamsConfig>::CallbackType f_;
+
+  f_ = boost::bind(&dynamicReconfigureCallback,_1,_2);
+  server_.setCallback(f_);
+
   while (ros::ok()) 
   {
     ros::spinOnce();  
@@ -34,3 +43,4 @@ int main(int argc, char** argv) {
   return 0;
   
 }
+
