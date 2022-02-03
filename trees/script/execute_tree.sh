6d4a186676f345d7e82c5c5b2c8e2a18d3fe08a8
simon <simon.martinez.rzs@gmail.com>
2022-02-03 22:58:30 +0100

    Adding boolean option for distance_function

--- trees/script/execute_tree.sh
+++ trees/script/execute_tree.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
 
 arg1="$1"
-echo "$arg1"
+arg2="$2"
 
-path1="/home/simon/marsupial_ws/src/marsupial_optimizer/trees/"
+echo "Set parameter: <pc_user_name> <tree_name>"
+echo "    pc_user_name: $arg1"
+echo "    tree_name: $arg2"
 
-full_path="$path1$arg1"
+path="/home/$arg1/marsupial_ws/src/marsupial_optimizer/trees/"
+
+full_path="$path$arg2"
+echo "full_path: $full_path"
 rosservice call /behavior_tree/load_tree "tree_file: '$full_path'"
