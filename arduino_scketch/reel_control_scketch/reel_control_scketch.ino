f596d9d4a716c22512db936d605a1fcdde330442
david-alejo <daletei@upo.es>
2022-07-26 18:45:16 +0200

    Simplified version tested

--- arduino_scketch/reel_control_scketch/reel_control_scketch.ino
+++ arduino_scketch/reel_control_scketch/reel_control_scketch.ino
@@ -22,8 +22,9 @@ float current_L = 1.1; //initial length
 float ref_L = 0; // reference length to control  
 float PWM = 0;
 bool controlling = false;
+bool reverse = false;
 unsigned long current_time,  delta_time;
-unsigned long previus_time = 0;
+unsigned long previous_time = 0;
 
 // ROS stuff
 ros::NodeHandle  nh;
@@ -33,66 +34,90 @@ std_msgs::UInt16 count_steps_turn_motor_msg;
 
 // Callbacks
 void lengthSubCallback(const std_msgs::Float32& length_ref_msg) {
-    ref_L = length_ref_msg.data;
-    count = 0;
-    controlling  = true;
+    if (fabs(ref_L - length_ref_msg.data) > 0.02) { 
+      ref_L = length_ref_msg.data;
+      count = 0;
+      controlling  = true;
+      nh.loginfo("Received new length command: ");
+    }
+    
 }
 
 void resetLengthCallback(const std_msgs::Float32& length_reset) {
      current_L = initial_L = length_reset.data;
+     controlling = false;
+     count = 0;
 }
 
 void enableCallback(const std_msgs::Bool& bool_msg) {
     enable = bool_msg.data;
     if (enable) {
+        controlling = false;
         nh.loginfo("Control enabled");
     } else {
         nh.loginfo("Control disabled");
     }
+    count = 0;
+}
+
+void reverseCallback(const std_msgs::Bool& bool_msg) {
+    reverse = bool_msg.data;
+    if (reverse) {
+        nh.loginfo("Reverse enabled");
+    } else {
+        nh.loginfo("Reverse disabled");
+    }
+    controlling = false;
+    count = 0;
 }
 
 
-
 // Subscriber for the length command
-ros::Subscriber<std_msgs::Float32> sub("set_length", &lengthSubCallback );
+ros::Subscriber<std_msgs::Float32> sub("control_tie/set_length", &lengthSubCallback );
 
 // Emit the current estimated longitude of the tie
-ros::Publisher pub_length("length_status", &length_msg);
+ros::Publisher pub_length("control_tie/length_status", &length_msg);
 
 // Wait for the message to start the system
-ros::Subscriber<std_msgs::Bool> sub_enable("enable", &enableCallback);
+ros::Subscriber<std_msgs::Bool> sub_enable("control_tie/enable", &enableCallback);
 
 // Length reset topic just in case
-ros::Subscriber<std_msgs::Float32> sub_reset("reset_length_estimation", &resetLengthCallback);
+ros::Subscriber<std_msgs::Float32> sub_reset("control_tie/reset_length_estimation", &resetLengthCallback);
+
+// Reverse the control and estimation
+ros::Subscriber<std_msgs::Bool> sub_reverse("control_tie/reverse", &reverseCallback);
 
 // Para depurar el funcionamiento del cacharro
-ros::Publisher pub_count_steps_turn_motor("count_steps_turn_motor", &count_steps_turn_motor_msg); 
+ros::Publisher pub_count_steps_turn_motor("control_tie/count_steps_turn_motor", &count_steps_turn_motor_msg); 
 
 void controlReel(float ref_L_){
   // put your main code here, to run repeatedly:
   float error = ref_L_ - current_L;
-  float tolerance_error = 0.01;
-  int sign;
+  if (reverse)
+    error *= -1.0;
+  float tolerance_error = 0.02;
+  int sign = 1; // If no control action an action would increase the longitude
    
   if (error < tolerance_error && error > -tolerance_error && controlling){
     PWM = 0;
     controlling == false;
     initial_L = current_L;
     sign = 0;
+    count = 0;
   }
   else if (error > 0 && controlling){ // increase length: Max_Vel=98 pwm , Min_Vel=118 pwm ; slope = -20 
     if (error > 1.0)
       PWM = 98;
     else
       PWM = -20 * error + 118;
-    sign = 1.0;
+    sign = reverse?-1.0:1.0;
   }
   else if (error < 0 && controlling){ // reduce lentgh: Max_Vel=195 pwm , Min_Vel=175 pwm ; slope = -20
     if (error < -1.0)
       PWM = 195;
     else
       PWM = -20 * error + 175;
-    sign = -1.0;
+    sign = reverse?1.0:-1.0;
   }
   analogWrite(servoPin, PWM);
       
@@ -102,8 +127,8 @@ void controlReel(float ref_L_){
 
 void countPulse(){
   current_time = millis();
-  delta_time = current_time - previus_time;
-  previus_time = current_time;
+  delta_time = current_time - previous_time;
+  previous_time = current_time;
   if (delta_time < 20)
     count++;
   count_steps_turn_motor_msg.data = count;
@@ -126,6 +151,7 @@ void setup() {
     nh.subscribe(sub);
     nh.subscribe(sub_enable);
     nh.subscribe(sub_reset);
+    nh.subscribe(sub_reverse);
 }
 
 void loop() { 
