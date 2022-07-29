826fb89cbd002afd8f8b4197ec1c2a609309a6e5
david-alejo <daletei@upo.es>
2022-07-29 20:58:30 +0200

    Calibrated, error +- 10cm/5m. Added one way

--- arduino_scketch/reel_control_scketch/reel_control_scketch.ino
+++ arduino_scketch/reel_control_scketch/reel_control_scketch.ino
@@ -12,7 +12,7 @@ int servoPin = 6;        // servo motor connected to digital pin 6
 int ledPin = 13;        // The 13th pin is connected to a LED
 int enable = 0;
 int count = 0;
-float diameter = 0.115;
+float diameter = 0.15;
 float delta_L = 0.0;
 float total_pulses = 1150.0;
 float L_min = 1.0; // minimum length allowed 
@@ -22,7 +22,7 @@ float current_L = 1.1; //initial length
 float ref_L = 0; // reference length to control  
 float PWM = 0;
 bool controlling = false;
-bool reverse = false;
+bool one_way = false;
 unsigned long current_time,  delta_time;
 unsigned long previous_time = 0;
 
@@ -31,6 +31,10 @@ ros::NodeHandle  nh;
 
 std_msgs::Float32 length_msg;
 std_msgs::UInt16 count_steps_turn_motor_msg;
+std_msgs::Bool length_reached_msg;
+
+// Let the mission controller know that the cable as reached the desired length
+ros::Publisher pub_length_reached("tie_controller/length_reached", &length_reached_msg);
 
 // Callbacks
 void lengthSubCallback(const std_msgs::Float32& length_ref_msg) {
@@ -39,6 +43,9 @@ void lengthSubCallback(const std_msgs::Float32& length_ref_msg) {
       count = 0;
       controlling  = true;
       nh.loginfo("Received new length command: ");
+      initial_L = current_L;
+      length_reached_msg.data = 0;
+      pub_length_reached.publish(&length_reached_msg);
     }
     
 }
@@ -54,18 +61,19 @@ void enableCallback(const std_msgs::Bool& bool_msg) {
     if (enable) {
         controlling = false;
         nh.loginfo("Control enabled");
+        initial_L = current_L;
     } else {
         nh.loginfo("Control disabled");
     }
     count = 0;
 }
 
-void reverseCallback(const std_msgs::Bool& bool_msg) {
-    reverse = bool_msg.data;
-    if (reverse) {
-        nh.loginfo("Reverse enabled");
+void one_wayCallback(const std_msgs::Bool& bool_msg) {
+    one_way = bool_msg.data;
+    if (one_way) {
+        nh.loginfo("one_way enabled");
     } else {
-        nh.loginfo("Reverse disabled");
+        nh.loginfo("one_way disabled");
     }
     controlling = false;
     count = 0;
@@ -73,51 +81,58 @@ void reverseCallback(const std_msgs::Bool& bool_msg) {
 
 
 // Subscriber for the length command
-ros::Subscriber<std_msgs::Float32> sub("control_tie/set_length", &lengthSubCallback );
+ros::Subscriber<std_msgs::Float32> sub("tie_controller/set_length", &lengthSubCallback );
 
 // Emit the current estimated longitude of the tie
-ros::Publisher pub_length("control_tie/length_status", &length_msg);
+ros::Publisher pub_length("tie_controller/length_status", &length_msg);
 
 // Wait for the message to start the system
-ros::Subscriber<std_msgs::Bool> sub_enable("control_tie/enable", &enableCallback);
+ros::Subscriber<std_msgs::Bool> sub_enable("tie_controller/enable", &enableCallback);
 
 // Length reset topic just in case
-ros::Subscriber<std_msgs::Float32> sub_reset("control_tie/reset_length_estimation", &resetLengthCallback);
+ros::Subscriber<std_msgs::Float32> sub_reset("tie_controller/reset_length_estimation", &resetLengthCallback);
 
-// Reverse the control and estimation
-ros::Subscriber<std_msgs::Bool> sub_reverse("control_tie/reverse", &reverseCallback);
+// one_way the control and estimation
+ros::Subscriber<std_msgs::Bool> sub_one_way("tie_controller/one_way", &one_wayCallback);
+
+
 
 // Para depurar el funcionamiento del cacharro
-ros::Publisher pub_count_steps_turn_motor("control_tie/count_steps_turn_motor", &count_steps_turn_motor_msg); 
+ros::Publisher pub_count_steps_turn_motor("tie_controller/count_steps_turn_motor", &count_steps_turn_motor_msg); 
+
+ros::Publisher pub_lenth_reached("tie_controller/length_reached", &length_reached_msg);
 
 void controlReel(float ref_L_){
   // put your main code here, to run repeatedly:
   float error = ref_L_ - current_L;
-  if (reverse)
-    error *= -1.0;
   float tolerance_error = 0.02;
   int sign = 1; // If no control action an action would increase the longitude
    
   if (error < tolerance_error && error > -tolerance_error && controlling){
     PWM = 0;
-    controlling == false;
+    controlling = false;
     initial_L = current_L;
     sign = 0;
     count = 0;
+    length_reached_msg.data = true;
+    pub_length_reached.publish(&length_reached_msg);
   }
   else if (error > 0 && controlling){ // increase length: Max_Vel=98 pwm , Min_Vel=118 pwm ; slope = -20 
     if (error > 1.0)
       PWM = 98;
     else
       PWM = -20 * error + 118;
-    sign = reverse?-1.0:1.0;
+    sign = 1.0;
   }
-  else if (error < 0 && controlling){ // reduce lentgh: Max_Vel=195 pwm , Min_Vel=175 pwm ; slope = -20
+  else if ( error < 0 && controlling){ // reduce lentgh: Max_Vel=195 pwm , Min_Vel=175 pwm ; slope = -20
     if (error < -1.0)
       PWM = 195;
     else
       PWM = -20 * error + 175;
-    sign = reverse?1.0:-1.0;
+    sign = -1.0;
+
+    if (one_way)
+      PWM = 0; // In one way mode the controller can only increase the length of the tie
   }
   analogWrite(servoPin, PWM);
       
@@ -148,10 +163,11 @@ void setup() {
     nh.initNode();
     nh.advertise(pub_length);
     nh.advertise(pub_count_steps_turn_motor);
+    nh.advertise(pub_length_reached);
     nh.subscribe(sub);
     nh.subscribe(sub_enable);
     nh.subscribe(sub_reset);
-    nh.subscribe(sub_reverse);
+    nh.subscribe(sub_one_way);
 }
 
 void loop() { 
